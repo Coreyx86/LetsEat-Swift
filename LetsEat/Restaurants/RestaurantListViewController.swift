162356b9fc2aa4b131e465d7b418ca4ecb205166
Coreyx86 <curreysauxe@gmail.com>
2020-10-26 19:04:46 -0700

    Merge project folder into repo
    Not sure why it wasn't included in the first attempt to commit this lol

---
+++ LetsEat/Restaurants/RestaurantListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantListViewController.swift
+//  LetsEat
+//
+//  Created by corey on 10/26/20.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
