d52d7a54ca509ebc9a72b0223c0b0af42832f3d5
Coreyx86 <curreysauxe@gmail.com>
2020-10-28 15:53:13 -0700

    Connect Explore MVC components

---
+++ LetsEat/Explore/View/ExploreCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExploreCell.swift
+//  LetsEat
+//
+//  Created by corey on 10/28/20.
+//
+
+import UIKit
+
+class ExploreCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var imgExplore: UIImageView!
+}
